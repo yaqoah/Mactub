5c994e4bc381916c15527389c15105926bdf8f17
Ahmed Awad <balotelli-7amood@hotmail.com>
2021-08-25 14:53:12 +0100

    Time to read text

--- mactub/tessera.py
+++ mactub/tessera.py
@@ -1,20 +1,27 @@
 from PIL import Image
 from pytesseract import pytesseract
 import sys
+import re
 
 tesseract_path = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+READ_TIME = 240
+WORD_COUNT = 500
 
-
-def read_by_path(image_path):
-    global tesseract_path
-    brief_img = ""
+def read(image_path):
+    img = ""
 
     try:
-        brief_img = Image.open(image_path)
+        img = Image.open(image_path)
     except FileNotFoundError as f:
         sys.exit(0)
 
     pytesseract.tesseract_cmd = tesseract_path
-    text = pytesseract.image_to_string(brief_img)
+    text = pytesseract.image_to_string(img)
+    clean_text = text[:-1].replace("\n\n", "\n")
 
-    print(text[:-1])
+    return clean_text
+
+
+def duration(text):
+    words_count = len(re.findall(r'\w+', text))
+    return (words_count*READ_TIME)/WORD_COUNT
