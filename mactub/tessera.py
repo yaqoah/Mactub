5ce1e45b3f96ca7d4155eb5d4caa88bec687adbf
Ahmed Awad <balotelli-7amood@hotmail.com>
2021-08-25 17:03:55 +0100

    Create image -> read -> calc duration to read

--- mactub/tessera.py
+++ mactub/tessera.py
@@ -1,5 +1,6 @@
 from PIL import Image
 from pytesseract import pytesseract
+
 import sys
 import re
 
@@ -7,14 +8,19 @@ tesseract_path = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 READ_TIME = 240
 WORD_COUNT = 500
 
-def read(image_path):
-    img = ""
+
+def create(image_path):
+    img = None
 
     try:
         img = Image.open(image_path)
     except FileNotFoundError as f:
-        sys.exit(0)
+        sys.exit(1)
 
+    return img
+
+
+def read(img):
     pytesseract.tesseract_cmd = tesseract_path
     text = pytesseract.image_to_string(img)
     clean_text = text[:-1].replace("\n\n", "\n")
@@ -23,5 +29,6 @@ def read(image_path):
 
 
 def duration(text):
+    # this can be used to calculate the duration of every line and sleep then print
     words_count = len(re.findall(r'\w+', text))
-    return (words_count*READ_TIME)/WORD_COUNT
+    return (words_count * READ_TIME) / WORD_COUNT
