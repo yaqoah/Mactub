1ac90379b0a8591d4cf6b1df2bc4c47882712cd5
Ahmed Awad <balotelli-7amood@hotmail.com>
2021-08-26 17:48:18 +0100

    Slideshow gui time.sleep();; tested > failed

--- mactub/__main__.py
+++ mactub/__main__.py
@@ -4,28 +4,31 @@ import time
 import sys
 
 from tkinter import *
-from PIL import ImageTk
+from PIL import ImageTk, Image
 from book import Book
 from cover import Cover
 import tessera
 from __init__ import __version__
+
+showing = None
 
 
 def get_args():
     """get script CL arguments"""
 
     description = "Mactub - Access and view books downloaded " \
-                  "to assist in selecting by reading short synopsis"
+                  "in device,to assist in selecting " \
+                  "by reading short synopsis"
 
     parser = argparse.ArgumentParser(description=description)
 
     parser.add_argument("--width", metavar="'px'",
                         help="What width to display book cover in",
-                        default=2560)
+                        default=640)
 
     parser.add_argument("--height", metavar="'px'",
                         help="What height to display book cover in",
-                        default=1600)
+                        default=590)
 
     parser.add_argument("--books_path", metavar="'path/to/dir",
                         help="Path to folder containing "
@@ -44,12 +47,14 @@ def get_args():
     parser.add_argument("--version", action="store_true",
                         help="print Mactub version")
 
-    #may add one here for only image/synopsis
+    # may add one here for only image/synopsis
 
     return parser.parse_args()
 
 
-def slideshow(args, room, root):
+def slideshow(args, realm, root):
+    global showing
+
     mac = Book(args.books_path)
     mac.fetch()
     name = mac.get_title()
@@ -62,43 +67,49 @@ def slideshow(args, room, root):
     back_cover_path = tub.get_cover("back")
     back_cover_image = tessera.create(back_cover_path)
 
-    front_tk = ImageTk.PhotoImage(tessera.resize(front_cover_image,
-                                                 args.width,
-                                                 args.height))
-    room.config(image=front_tk)
+    root.showing = ImageTk.PhotoImage(tessera.resize(front_cover_image,args.width,args.height), master=root)
+    realm.config(image=root.showing)
     time.sleep(1.5)
 
-    back_tk = ImageTk.PhotoImage(tessera.resize(back_cover_image,
-                                                args.width,
-                                                args.height))
-    room.config(image=back_tk)
-    text_ls = tessera.read(back_cover_image).split("\n\n")
-    limit = 3
-    to_show = ""
-    for few_lines in text_ls:
+    showing = ImageTk.PhotoImage(tessera.resize(back_cover_image,args.width,args.height),master=root)
+    realm.config(image=root.showing)
+    text = tessera.read(back_cover_image)
 
-        if limit and text_ls:
-            to_show += few_lines
-            text_ls.remove(few_lines)
+    limit = 5
+    to_show = ""
+    for line in text:
+
+        if line.isspace():
+            to_show += " \n"
+            continue
+
+        if limit and text:
+            to_show += (line + " \n")
             limit -= 1
 
-        if not limit or not text_ls:
+        if not limit or not text:
             time_reading = tessera.duration(to_show)
             print(to_show)
-            time.sleep(time_reading/1000)
-            limit = 3
+            time.sleep(time_reading)
+            limit = 5
             to_show = ""
-    root.after(200, slideshow(args, room, root))
+
+    root.after(300, lambda: slideshow(args, realm, root))
 
 
 def main():
+    global showing
     args = get_args()
 
     root = Tk()
     root.geometry(str(args.width) + "x" + str(args.height))
-    room = Label()
-    room.pack()
-    root.after(200, slideshow(args, room, root))
+    root.showing = ImageTk.PhotoImage(tessera.resize(Image.open("heya.png"),
+                                                args.width,
+                                                args.height),
+                                 master=root)
+    realm = Label(root, image=root.showing)
+    realm.pack()
+    root.after(300, lambda: slideshow(args, realm, root))
     root.mainloop()
 
 
