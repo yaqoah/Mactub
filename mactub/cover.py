12afea9e944d28a8741f4a48180668aca70641e1
Ahmed Awad <balotelli-7amood@hotmail.com>
2021-08-27 22:46:18 +0100

    Correcting double casting same data type for pathlib obj

--- mactub/cover.py
+++ mactub/cover.py
@@ -7,21 +7,29 @@ from exceptions import MissingHardDisk
 
 
 class Cover:
-    """Get back and front paths"""
+    """Get back and front cover (by path)"""
 
     def __init__(self, book, bindings):
+        """Initialise cover (of book) object"""
+
         self.book = book
 
         try:
-            self.book_path = pathlib.Path(bindings)
-            if not self.book_path.exists():
-                raise MissingHardDisk("Plugin your 'D' drive")
+            self.path_to_cover = bindings
+            if not self.path_to_cover.exists():
+                raise MissingHardDisk(bindings, "Plugin your 'D' drive")
 
         except MissingHardDisk as hdd:
             sys.exit("1)Check cover path.\n2)" + str(hdd))
 
     def get_cover(self, face):
-        p = self.book_path.glob('*')
+        """
+        Get cover of book by specified face
+        :param face: what side of cover (front or back)
+        :return: path to face specified (front/back)
+        """
+
+        p = self.path_to_cover.glob('*')
         bindings = [cover for cover in p]
         book_acronym = ''.join([word[0].upper()
                                 for word in
@@ -31,12 +39,12 @@ class Cover:
             if re.search(book_acronym, binding.stem):
                 if face == "front":
                     if binding.stem[-1] == "0":
-                        return pathlib.Path(str(self.book_path)
+                        return pathlib.Path(str(self.path_to_cover)
                                             + "/"
                                             + binding.name)
 
                 else:
                     if binding.stem[-1] == "1":
-                        return pathlib.Path(str(self.book_path)
+                        return pathlib.Path(str(self.path_to_cover)
                                             + "/"
                                             + binding.name)
