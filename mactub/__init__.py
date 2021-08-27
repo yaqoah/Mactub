f66520a428c4798daa81f34164777f38f395ba20
Ahmed Awad <balotelli-7amood@hotmail.com>
2021-08-27 22:42:20 +0100

    __version__ > 0.0.1

--- mactub/__init__.py
+++ mactub/__init__.py
@@ -11,17 +11,4 @@
 ~     مكتب     ~
 by Ahmed Yousif
 """
-# add version
-from cover import Cover
-from book import Book
-from tessera import read_by_path
-
-btest = Book()
-btest.fetch()
-book = btest.get_title()
-print(book)
-
-test = Cover(book)
-print(test.get_cover("front"))
-print(test.get_cover("back"))
-read_by_path(test.get_cover("back"))
+__version__ = "0.0.1"
