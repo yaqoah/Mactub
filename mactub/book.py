11a14d687ebf5b9d151e3056d2c96324ac9c7131
Ahmed Awad <balotelli-7amood@hotmail.com>
2021-08-27 22:45:53 +0100

    Correcting double casting same data type for pathlib obj

--- mactub/book.py
+++ mactub/book.py
@@ -13,19 +13,23 @@ class Book:
     """Book infos are to be known"""
 
     def __init__(self, shelf):
+        """Initialize book object """
+
         self.exhibited = []
         self.title = ""
         self.author = ""
 
         try:
-            self.path = pathlib.Path(shelf)
+            self.path = shelf
             if not self.path.exists():
-                raise MissingHardDisk("Plugin your 'D' drive")
+                raise MissingHardDisk(shelf, "Plugin your 'D' drive")
 
         except MissingHardDisk as hdd:
             sys.exit("1)Check book path.\n2)" + str(hdd))
 
     def fetch(self):
+        """Obtain book title and author from path data in directory"""
+
         p = self.path.glob('*')
         books = [book for book in p if book not in self.exhibited]
 
@@ -41,7 +45,15 @@ class Book:
             self.fetch()
 
     def get_title(self):
+        """
+        :return: title of book
+        """
+
         return self.title
 
     def get_author(self):
+        """
+        :return: author name of book
+        """
+
         return self.author
