de6ed142ea64076b7a3870feeb6656090df45906
Ahmed Awad <balotelli-7amood@hotmail.com>
2021-08-28 22:06:04 +0100

    General: setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Mactub',
+    version='VERSION',
+    packages=['mactub'],
+    url='https://github.com/yaqoah/Mactub',
+    license='MIT',
+    author='Ahmed Yousif',
+    author_email='balotelli-7amood@hotmail.com',
+    description='View books in a device, read and print synopsis in back'
+)
